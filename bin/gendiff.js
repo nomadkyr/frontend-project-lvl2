02f388b72db41e8193225069d4e38f9bb35fb276
Daria <dariasid04@gmail.com>
2022-08-03 14:22:32 +0600

    step three

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
-program 
+program
   .name('gendiff')
+  .usage('[options] <filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .version('3.4.4')  
-  .description('Compares two configuration files and shows a difference.');
-  
- 
+  .version('3.4.4')
+  .description('Compares two configuration files and shows a difference.')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 program.parse();
